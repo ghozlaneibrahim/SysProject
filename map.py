27779e46fdf5772f132a88068729896b26485c72
ibrahim ghozlane <ibrahimghozlane2000@gmail.com>
2021-12-17 23:33:49 +0100

    1st step done

--- map.py
+++ map.py
@@ -2,36 +2,53 @@ import folium
 from folium.map import Tooltip
 from geopy.geocoders import Nominatim
 import os
+import json
 
 
 ## fog hadi ndiro win lazm user ymed les images w  7na njbdo les cites
-## hna yjbed city w cooridnation ta3hom w ykhdmlhom markers
-geolocator = Nominatim(user_agent="CityFinder")
-location = geolocator.geocode("alger")
+style2 = {"fillColor": "#228B22", "color": "#eedcdd"}
+locations = []
+## hado les cites li ra7 n7wso 3lihom ki shkopi mohim dert tableau ana bash ntesto brk
 
-style2 = {'fillColor': '#228B22', 'color': '#eedcdd'}
+for city in ["Mekka", "alger", "Berlin"]:
+    geolocator = Nominatim(user_agent="CityFinder")
+    ## hna yjbed city w cooridnation ta3hom
+    locations.append(geolocator.geocode(city))
+
+
 # create map object
-print(location)
-print(location.latitude, location.longitude)
 
+location = locations[0]
+print(locations[2])
 
+# hna yakhdm map
 m = folium.Map(
     location=[location.latitude, location.longitude],
-    tiles="http://tile.stamen.com/watercolor/{z}/{x}/{y}.jpg'",
-    zoom_size=2.5,
+    tiles="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+    zoom_size=40,
+    zoom_start = 3,
     attr="My Data Attribution",
 )
 
+f = open("maroc.json")
 
-overlay = os.path.join('maroc.geojson')
 
-folium.GeoJson(overlay, style_function=lambda x:style2).add_to(m)
+data = json.loads(f.read())
+citiesCord = data["features"][0]["geometry"]["coordinates"][0]
+for i in range(0, len(locations)):
+    data["features"][0]["geometry"]["coordinates"][0].append(
+        [locations[i].longitude, locations[i].latitude]
+    )
 
-folium.Marker(
-    [location.latitude, location.longitude],
-    popup="<strong> location </strong>",
-    icon=folium.Icon(color="red"),
-).add_to(m)
+for cityCord in citiesCord:
+
+    folium.GeoJson(data, style_function=lambda x: style2).add_to(m)
+
+    folium.Marker(
+        [cityCord[1], cityCord[0]],
+        popup="<strong> location </strong>",
+        icon=folium.Icon(color="red"),
+    ).add_to(m)
 
 
 # w hna t7t ydir surface w yrsom tmnkii7 hadak w nchlh nkono kmlna
